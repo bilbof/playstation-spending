37b515d3ab3cce15cbeedb5bc8a06a01a6ae64e9
Bill Franklin <b.franklin@outlook.com>
2025-01-14 14:53:23 +0000

    Show PlayStation store spending, subtotal by year, and total

---
+++ README.md
@@ -0,0 +1,29 @@
+# PlayStation store spending
+
+Calculate how much you spent on Playstation per year and export transactions.
+
+## Output example
+
+```
+python ./line-items.py
+15-04-2024 £2.99  Firewatch
+12-05-2024 £19.99 ANIMAL WELL
+10-06-2024 £2.49  Sid Meier’s Civilization VI
+------------------------------
+Subtotal by year
+------------------------------
+2023: £123.45
+2024: £123.45
+2025: £123.45
+=============================
+Total spend: £1234.56
+=============================
+```
+
+## How to use:
+
+1. Sign in to PlayStation in a web browser
+2. Go to https://web.np.playstation.com/api/graphql/v1/transact/transaction/history?limit=250&startDate=2018-01-01T00%3A00%3A00.000%2B0000&endDate=2025-12-31T16%3A54%3A59.663%2B0100&includePurged=false&transactionTypes=CREDIT,CYCLE_SUBSCRIPTION,DEBIT,DEPOSIT_CHARGE,DEPOSIT_VOUCHER,PRODUCT_PURCHASE,REFUND_PAYMENT_CHARGE,REFUND_PAYMENT_WALLET,VOUCHER_PURCHASE,WALLET_BALANCE_CONVERSION
+  Note: you can increase the limit and change the date range in the query params. If hasMore: true in the response, do multiple requests
+3. Download the JSON file response and put it into a top-level "./transactions" directory.
+4. Run `python line-items.json`
---
+++ line-items.py
@@ -0,0 +1,50 @@
+from os import listdir
+from os.path import isfile, join, exists
+import json
+from pprint import pprint
+from dateutil.parser import parse
+
+transaction_files_dir = "./transactions"
+if not exists(transaction_files_dir):
+  print("ERROR: You need to download the transactions and put them in ./transactions")
+  exit(1)
+
+transaction_files = [f"{transaction_files_dir}/{f}" for f in listdir(transaction_files_dir) if isfile(join(transaction_files_dir, f))]
+transactions = []
+
+for name in transaction_files:
+  print(name)
+  with open(name) as f:
+    transactions += json.load(f)["transactions"]
+
+transactions = sorted(transactions, key=lambda t: parse(t["transactionDetail"]["transactionDate"]))
+
+def pad(s):
+  if len(s) < 6:
+    return s + (" " * (6 - len(s)))
+  return s
+
+total_per_year = {}
+
+print("Transactions")
+print("------------------------------")
+for t in transactions:
+  date = parse(t["transactionDetail"]["transactionDate"])
+  line_items = t.get("additionalInfo", {}).get("orderItems", [])
+  for item in line_items:
+    val = item["totalPrice"]["value"]
+    total_per_year[date.year] = total_per_year.get(date.year, 0) + val
+    if val > 0:
+      print(f"{date.strftime('%d-%m-%Y')} {pad(item['totalPrice']['formattedValue'])} {item['productName']}")
+
+print("------------------------------")
+print("Subtotal by year")
+print("------------------------------")
+total = 0
+for year, amount in total_per_year.items():
+  total += amount
+  print(f"{year}: £{amount / 100}")
+
+print("=============================")
+print(f"Total spend: £{total / 100}")
+print("=============================")
